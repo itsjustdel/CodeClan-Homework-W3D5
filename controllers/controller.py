5522467d00738ed09494c2defd066d28fbce9323
Derek Reid <itsjustdel@gmail.com>
2021-11-26 18:32:33 +0000

    form POST working

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,9 +6,11 @@ from models.game import *
 
 @app.route('/game/')
 def index():
+    # going home, clear list of players, we will re-populate on next game
+    players.clear()
     return render_template('index.html', title='Rock, Paper, Scissors!')
 
-@app.route('/game/<choice1>/<choice2>')
+@app.route('/game/<choice1>/<choice2>/')
 def play_game(choice1,choice2):
     
     player1 = Player("Agamemnon", choice1)
@@ -19,20 +21,17 @@ def play_game(choice1,choice2):
 
     return render_template('outcome.html',player_list=player_list, winning_player=winning_player)
 
-@app.route('/pvp/')
+@app.route('/pvp/') #add var for button name?
 def pvp():
     return render_template('pvp.html')
 
-@app.route('/outcome/', methods=['POST'])
+@app.route('/add_player', methods=['POST'])
 def add_player():
     player = Player(request.form['name'], request.form['choice'])
-    # add to our list we have declared in player_list.py
+    # add to our list we have declared in player_list.py    
     players.append(player)
-    players.append(player)
 
-    #go back to add player
-    return index()
-
-
-
-
+    if(len(players) == 2):
+        return render_template('outcome.html',player_list=players, winning_player=winner(players[0],players[1]))
+    else:
+        return pvp()
